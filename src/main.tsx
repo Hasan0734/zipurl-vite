1492f3826c5f5bb9c32e64a295e5d7256d0759b1
Hasan0734 <jahidhasan2856@gmail.com>
2026-04-28 23:10:18 +0600

    added react-router

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,30 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
+import {createBrowserRouter, RouterProvider} from 'react-router'
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App/>
+    },
+    {
+      path: '/sign-in',
+      element: <h2>sign-in page</h2>
+    },
+        {
+      path: '/sign-up',
+      element: <h2>sign-up page</h2>
+    }
+  ])
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router}/>
+      {/* <App/> */}
     </ThemeProvider>
   </StrictMode>,
 );
