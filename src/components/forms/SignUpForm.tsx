f375208f577b074a4bd5ac35632647e7a715544c
Hasan0734 <jahidhasan2856@gmail.com>
2026-05-04 15:44:24 +0600

    adding the user register function

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -1,88 +1,141 @@
-import { ArrowRight, AtSign, Lock, ShieldCheck, User } from "lucide-react"
-import React from "react"
-import TextInput from "../TextInput"
-import { Button } from "../ui/button"
-import { Checkbox } from "../ui/checkbox"
+import { ArrowRight, AtSign, Lock, ShieldCheck, User } from "lucide-react";
+import React, { useState, useTransition } from "react";
+import TextInput from "../TextInput";
+import { Button } from "../ui/button";
+import { Checkbox } from "../ui/checkbox";
+import { FieldGroup } from "../ui/field";
+import { useForm } from "@tanstack/react-form";
+import { useNavigate } from "react-router";
+import api from "@/lib/api";
+import { Spinner } from "../ui/spinner";
+import {
+  registerUserSchema,
+  type RegisterUserSchemaType,
+} from "@/schema/user.schema";
+
+const userRegister = async (data: RegisterUserSchemaType) => {
+  try {
+    const res = await api.post("/auth/sign-in", data);
+    return res.data;
+  } catch (err: any) {
+    return err.response.data;
+  }
+};
 
 const SignUpForm = () => {
-  return (
-    <form action="#" className="space-y-6">
-      {/* <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <div className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-4">
-          <TextInput
-            label="First Name"
-            id="first_name"
-            icon={User}
-            placeholder="Alex"
-          />
-          <TextInput
-            label="Last Name"
-            id="last_name"
-            icon={User}
-            placeholder="Revra"
-          />
-        </div>
+  const [isPending, startTransition] = useTransition();
+  const navigate = useNavigate();
+  const [message, setMessage] = useState();
 
+  const form = useForm({
+    defaultValues: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    },
+    validators: {
+      onSubmit: registerUserSchema,
+    },
+    onSubmit: async ({ value }) => {
+      startTransition(async () => {
+        const data = await userRegister(value);
+
+        if (!data.success) {
+          setMessage(data.message);
+          return;
+        }
+        navigate("/sign-in", { replace: true });
+        form.reset();
+      });
+    },
+  });
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        form.handleSubmit();
+      }}
+      className="space-y-6"
+    >
+      <FieldGroup className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <TextInput
+          label="First Name"
+          id="first_name"
+          icon={User}
+          placeholder="Alex"
+          form={form}
+          className="col-span-2 md:col-span-1"
+        />
+        <TextInput
+          label="Last Name"
+          id="last_name"
+          icon={User}
+          placeholder="Revra"
+          form={form}
+          className="col-span-2 md:col-span-1"
+        />
         <TextInput
           label="Email"
+          name="email"
           id="email"
           icon={AtSign}
           placeholder="name@company.com"
           type="email"
+          form={form}
           className="col-span-2"
         />
 
         <TextInput
-          className="col-span-2 sm:col-span-1"
           label="Password"
+          name="password"
           id="password"
           icon={Lock}
           placeholder="••••••••"
           type="password"
+          form={form}
+          className="col-span-2"
         />
         <TextInput
-         className="col-span-2 sm:col-span-1"
           label="Confirm Password"
+          name="confirm_password"
           id="confirm_password"
-          icon={ShieldCheck}
+          icon={Lock}
           placeholder="••••••••"
           type="password"
+          form={form}
+          className="col-span-2"
         />
-      </div> */}
-      <div className="flex items-start gap-3 px-1">
-        <div className="flex h-5 items-center">
-          <Checkbox id="terms" />
+        <div className="flex items-start gap-3 px-1 col-span-2 ">
+          <div className="flex h-5 items-center">
+            <Checkbox name="terms" id="terms" />
+          </div>
+          <label className=" text-sm leading-tight" htmlFor="terms">
+            I agree to the{" "}
+            <a
+              className="text-primary underline-offset-4 hover:underline"
+              href="#"
+            >
+              Terms of Service
+            </a>{" "}
+            and{" "}
+            <a
+              className="text-primary underline-offset-4 hover:underline"
+              href="#"
+            >
+              Privacy Policy
+            </a>
+            .
+          </label>
         </div>
-        <label
-          className="text-on-surface-variant text-sm leading-tight"
-          htmlFor="terms"
-        >
-          I agree to the{" "}
-          <a
-            className="text-primary underline-offset-4 hover:underline"
-            href="#"
-          >
-            Terms of Service
-          </a>{" "}
-          and{" "}
-          <a
-            className="text-primary underline-offset-4 hover:underline"
-            href="#"
-          >
-            Privacy Policy
-          </a>
-          .
-        </label>
-      </div>
-      <Button
-        size={'lg'}
-        className="w-full h-11!"
-        type="submit"
-      >
-        Create account <ArrowRight />
+      </FieldGroup>
+
+      <Button size={"lg"} className="w-full h-11!" type="submit">
+        {isPending && <Spinner />} Create account
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -12,3 +12,28 @@ export const loginUserSchema = z.object({
 export type LoginUserSchemaType = z.infer<typeof loginUserSchema>;
 
 
+const userInfo = z.object({
+    first_name: z.string().min(2, "First name too short.").max(50, "First name too long."),
+    last_name: z.string().min(2, "First name too short.").max(50, "First name too long."),
+    email: z
+        .email("Please enter a valid email address")
+        .trim()
+        .max(255)
+        .toLowerCase()
+
+});
+
+const passwordSchema = z.object({
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirm_password: z.string(),
+})
+    .refine((data) => data.password === data.confirm_password, {
+        message: "Passwords don't match",
+        path: ["confirm_password"],
+    });
+
+export const registerUserSchema = userInfo.merge(passwordSchema);
+
+export type RegisterUserSchemaType = z.infer<typeof registerUserSchema>;
+
+
